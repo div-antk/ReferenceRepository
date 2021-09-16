1a3381d2c37823f6a31acdb61633634dab802603
div-antk <51283468+owaii@users.noreply.github.com>
2021-09-16 19:48:49 +0900

    VMのテンプレートにコメントを追加

--- ReferenceRepository/ViewModel/ViewModel.swift
+++ ReferenceRepository/ViewModel/ViewModel.swift
@@ -9,36 +9,40 @@ import Foundation
 import RxSwift
 import RxCocoa
 
+// プロトコルを適用したクラスや構造体は、プロトコルに定義されているメソッド、プロパティを必ず実装しなければならない
 protocol ViewModelInputs {
-    
+    // getは読み込み専用プロパティを意味する
+
 }
 
 protocol ViewModelOutputs {
-    
+
 }
 
 protocol ViewModelType {
-    var inputs: ViewModelInputs {get}
-    var outputs: ViewModelOutputs {get}
+    var inputs: ViewModelInputs { get }
+    var outputs: ViewModelOutputs { get }
 }
 
 class ViewModel: ViewModelInputs, ViewModelOutputs {
-    
-    //MARK: -input
-    
-    //MARK: -output
-    
-    //MARK: -other
-    
+
+    // MARK: - input
+
+    // MARK: - output
+
+    // MARK: - other
+
     private let disposeBag = DisposeBag()
-    
+
+    // classのプロパティの初期値を設定する
+    // このクラスのインスタンスを生成する際に自動で呼び出される
     init() {
-        
+
     }
 
 }
 
 extension ViewModel: ViewModelType {
-    var inputs: ViewModelInputs {return self}
-    var outputs: ViewModelOutputs {return self}
+    var inputs: ViewModelInputs { self }
+    var outputs: ViewModelOutputs { self }
 }
