1b523703c144a03f3aad87debf3bcf20d15ff415
div-antk <51283468+owaii@users.noreply.github.com>
2021-09-17 23:47:18 +0900

    VCでCollectionViewの紐付け、セルの表示

--- ReferenceRepository/View/ViewController.swift
+++ ReferenceRepository/View/ViewController.swift
@@ -6,15 +6,38 @@
 //
 
 import UIKit
+import Instantiate
+import InstantiateStandard
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, StoryboardInstantiatable {
 
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var collectionView: UICollectionView!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        collectionView.delegate = self
+        collectionView.dataSource = self
+
+        collectionView.register(UINib(nibName: ItemCollectionViewCell.reusableIdentifier, bundle: nil), forCellWithReuseIdentifier: ItemCollectionViewCell.reusableIdentifier)
     }
 
+}
+
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        10
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ItemCollectionViewCell.reusableIdentifier, for: indexPath) as? ItemCollectionViewCell else { return UICollectionViewCell() }
+
+        cell.titleLabel.text = "タイトル"
+        cell.tagLabel.text = "タグです"
+
+        return cell
+    }
 }
