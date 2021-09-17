79a56af313b0422fdfbde8ad56c85541e5f02a03
div-antk <51283468+owaii@users.noreply.github.com>
2021-09-17 23:12:06 +0900

    カスタムセルを作成

---
+++ ReferenceRepository/View/Cell/ItemCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  ItemCollectionViewCell.swift
+//  ReferenceRepository
+//
+//  Created by Takuya Ando on 2021/09/17.
+//
+
+import UIKit
+
+class ItemCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var tagLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
