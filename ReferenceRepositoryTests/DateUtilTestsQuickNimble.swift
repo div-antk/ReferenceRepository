f8e82c7fd6d9645df4145a3d5241463eb83b485a
div-antk <51283468+owaii@users.noreply.github.com>
2021-10-20 10:46:45 +0900

    Quick/Nimbleを使用した場合のテストを記述

---
+++ ReferenceRepositoryTests/DateUtilTestsQuickNimble.swift
@@ -0,0 +1,30 @@
+//
+//  DateUtilTestsQuickNimble.swift
+//  ReferenceRepositoryTests
+//
+//  Created by Takuya Ando on 2021/10/20.
+//
+
+// Quick/Nimbleを使用した場合のテスト
+import Quick
+import Nimble
+
+@testable import ReferenceRepository
+
+class DateUtilTestsQuickNimble: QuickSpec {
+
+    override func spec() {
+
+        // テスト対象のメソッドを宣言
+        describe("dateStringConverter()") {
+
+            // どういう挙動になるべきか
+            it("日付が変換される") {
+
+                // Nimbleを使用してアサーションを記述
+                expect(DateUtil.dateStringConverter(string: "2021-01-01T00:00:00+09:00"))
+                    .to(equal("2021年01月01日"))
+            }
+        }
+    }
+}
